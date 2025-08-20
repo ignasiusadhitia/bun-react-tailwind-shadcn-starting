111df29db8d4592ded20e9b4335bc338be54402b
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2025-08-20 09:44:24 +0700

    style: set grid to 4 cols on xl display

--- src/projects/dragon-ball.tsx
+++ src/projects/dragon-ball.tsx
@@ -160,7 +160,7 @@ const DragonBallComponent: React.FC = () => {
         {/* Characters Grid */}
         <Suspense
           fallback={
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {[...Array(8)].map((_, index) => (
                 <CharacterSkeleton key={index} />
               ))}
@@ -168,13 +168,13 @@ const DragonBallComponent: React.FC = () => {
           }
         >
           {loading ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {[...Array(12)].map((_, index) => (
                 <CharacterSkeleton key={index} />
               ))}
             </div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {filteredCharacters.map((character) => (
                 <CharacterCard key={character.id} character={character} />
               ))}
